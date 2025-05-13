c7dadecdc07013e4f3311f3f7ce54e8fdcb92add
Jenn Milano <jmilano0354@gmail.com>
2025-05-12 21:50:05 -0400

    attemptt 1 to fix cors errors

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ client.connect()
     console.error('Error connecting to database:', err);
   });
 
-server.use(cors());
+server.use(cors({
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 server.use(morgan("dev"));
 server.use(express.json());
 
